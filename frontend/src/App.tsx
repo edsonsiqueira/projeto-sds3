0b1e7b1d71a1c0b868436d0faa6ab908ce246ab9
edson_siiqueira@hotmail.com <edson_siiqueira@hotmail.com>
2021-05-30 14:18:30 -0300

    Adicionando o NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import NavBar from 'components/NavBar';
 
 function App() {
   return (
     <div>
 
       <h1 className ="text-primary"> Olá Mundo!</h1>
+
+      <NavBar />
+
     </div>
   );
 }
